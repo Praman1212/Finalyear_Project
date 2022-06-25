803be37733810c42d54f2858558a7727136061b3
Praman <pramang017357@nec.edu.np>
2022-06-25 08:43:01 +0545

    update on login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -25,7 +26,15 @@ class AuthController extends Controller
         $user->save();
         return redirect('login');
     }
-    public function getUser(){
-        
+    public function getUser(Request $request){
+        $request->validate([
+            'email' =>'required',
+            'password'=> 'required'
+        ]);
+        $credentials = $request->only('email','password');
+        if(Auth::attempt($credentials)){
+            return redirect('/');
+        }
+        return redirect('/register');
     }
 }
